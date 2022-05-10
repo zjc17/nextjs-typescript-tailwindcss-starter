345f3e38bd24a9749b87be3f5455d64b3e45cf05
jiachen.zhang <zhangjc1999@gmail.com>
2022-05-10 21:27:33 +0800

    [Feature] init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+   
+module.exports = {
+  content: [
+    './src/components/**/*.{ts,tsx,js,jsx}', 
+    './src/pages/**/*.{ts,tsx,js,jsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
