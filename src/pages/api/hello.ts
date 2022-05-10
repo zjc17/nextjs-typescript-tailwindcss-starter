288ddad54d3709f9a0f9edddcea99f666fbe3292
jiachen.zhang <zhangjc1999@gmail.com>
2022-05-10 21:54:28 +0800

    [Bugfix] fix lint error

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'Jiachen Zhang' })
+var hello = function (_: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ text: 'Hello' })
 }
+
+export default hello
